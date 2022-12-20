286f302843362eec28606da0d9a09572fbd86ecd
JGNoraejarang <yuseung2004@gmail.com>
2022-12-20 09:56:22 +0900

    Update Hungry_Cancel.java

--- src/main/java/hc/hungry_cancel/Hungry_Cancel.java
+++ src/main/java/hc/hungry_cancel/Hungry_Cancel.java
@@ -7,6 +7,14 @@ import org.bukkit.event.Listener;
 import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 
+/**
+ * 이 플러그인은 대한민국의 개발자 JGNoraejarang에 의해 작성되었습니다.
+ * 공식 연락처는 Discord: JGNoraejarang#0796입니다.
+ *
+ * This plugin is written by JGNoraejarang, Programmer in Republic of Korea.
+ * Official Contact: Discord JGNoraejarang#0796
+ */
+
 public final class Hungry_Cancel extends JavaPlugin implements Listener{
 
     Logger logger = getServer().getLogger();
